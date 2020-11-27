f5e9c55360c986bd4527e0cab51d0604a9cfa572
fatih <f.kiyak@outlook.com>
2020-11-27 16:35:05 +0100

    Option1 added in branch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Begin hier met het bouwen van de applicatie!
-    </div>
-  );
-}
+    const [count, setCount] = React.useState(0);
 
+    const incrementCount = () => {
+        setCount(count + 1);
+        console.log('Increment count');
+    }
+
+    const decrementCount = () => {
+        setCount(count - 1);
+        console.log('Decrement count');
+    }
+
+    const resetCount = () => {
+        setCount(0);
+        console.log('Reset count');
+    }
+
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <div>
+                <button onClick={incrementCount}>+</button>
+                <button onClick={decrementCount}>-</button>
+                <button onClick={resetCount}>reset</button>
+            </div>
+        </>
+    );
+}
 export default App;
